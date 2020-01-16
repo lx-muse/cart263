7a15c7de1fb0c18275818744ecf13a7feee3a503
MC <lariviremc@gmail.com>
2020-01-16 13:39:05 -0500

    Challenge 1: player

--- CircleEaterChallenge1/js/script.js
+++ CircleEaterChallenge1/js/script.js
@@ -1,12 +1,26 @@
 /*****************
 
-Title of Project
-Author Name
+Circle Eater
+MC Lariviere
 
 This is a template. You must fill in the title,
 author, and this description to match your project!
 
 ******************/
+
+let player = {
+x: 0,
+y: 0,
+maxSize: 64,
+size: 64,
+active: true,
+}
+
+let food = {
+  x: 0,
+  y: 0,
+  size: 64,
+}
 
 // preload()
 //
@@ -22,7 +36,10 @@ function preload() {
 // Description of setup
 
 function setup() {
-
+createCanvas(windowWidth,windowHeight);
+food.x = random(0, width);
+food.y = random(0, height);
+noCursor();
 }
 
 
@@ -31,5 +48,24 @@ function setup() {
 // Description of draw()
 
 function draw() {
+  //darkpurple
+  background("#27164D");
+  updatePlayer();
+  displayPlayer();
+}
+
+function updatePlayer() {
+  player.x = MouseX;
+  player.y = MouseY;
+  if (player.size === 0){
+    player.active = false;
+  }
+}
+
+function displayPlayer() {
+  push();
+  noStroke();
+  fill("#fae")
+  ellipse(player.x, player.y, player.maxSize);
 
 }
