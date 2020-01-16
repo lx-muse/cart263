bcf988f428e5684d60fe8b76eef3bc3572eb2493
MC <lariviremc@gmail.com>
2020-01-16 16:04:08 -0500

    PixelPainter template

---
+++ PixerPainter/js/script.js
@@ -0,0 +1,11 @@
+"use strict";
+
+/********************************************************************
+
+Pixel Painter
+MC Lariviere
+
+This is a template. Fill in the title, author, and this description
+to match your project! Write JavaScript to do amazing things below!
+
+*********************************************************************/
